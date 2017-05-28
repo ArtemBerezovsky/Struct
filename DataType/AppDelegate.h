0f6844d3d2772735195dcffef30fe9ab2197fc19
ArtemBerezovsky <admin@mac-admin.local>
2017-05-28 11:37:14 +0300

    Initial Commit

---
+++ DataType/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataType
+//
+//  Created by Admin on 28/05/17.
+//  Copyright © 2017 Artem Berezovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataType/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataType
+//
+//  Created by Admin on 28/05/17.
+//  Copyright © 2017 Artem Berezovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
